c5be4995c305b0ef45d7ebc807d56e24fe0c4e8e
Lars Buntemeyer <g300046@btc0.bullx>
2015-02-04 13:24:01 +0100

    update

--- fortran/compiler/test.f90
+++ fortran/compiler/test.f90
@@ -13,8 +13,11 @@ d=4.0
 x = a*(b+c)
 y = a*b+a*c
 
-write(*,*) 'x:',x
-write(*,*) 'y:',y
-write(*,'(a10,e20.10)') '|x-y|/x:',abs(x-y)/x
+write(*,100) 'x:',x
+write(*,100) 'y:',y
+write(*,100) '|x-y|/x:',abs(x-y)/x
+
+100 format(a10,e20.10)
+101 format(e20.10)
 
 end program test
